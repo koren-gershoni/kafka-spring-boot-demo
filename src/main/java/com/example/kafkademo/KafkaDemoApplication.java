e77cb281a3e126ffb03a5f8294ca41c7c65a9a7f
koreng <korenger93@gmail.com>
2020-08-27 15:36:57 +0300

    publish message to kafka

--- src/main/java/com/example/kafkademo/KafkaDemoApplication.java
+++ src/main/java/com/example/kafkademo/KafkaDemoApplication.java
@@ -1,10 +1,19 @@
 package com.example.kafkademo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class KafkaDemoApplication {
+
+	private final KafkaTemplate<String, String> kafkaTemplate;
+
+	KafkaDemoApplication(KafkaTemplate<String, String> kafkaTemplate) {
+		this.kafkaTemplate = kafkaTemplate;
+		kafkaTemplate.send("example", "Hello World");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaDemoApplication.class, args);
